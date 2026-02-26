2211f2382539458faece774bf5811b0f4398bcf7
Ghada | غادة <ghadhalomar@gmail.com>
2026-02-26 12:15:19 +0300

    Add initial database schema structure

---
+++ database/schema.sql
@@ -0,0 +1,11 @@
+-- database/schema.sql
+
+-- ===== USERS (A) =====
+
+-- ===== TRAIN (B) =====
+
+-- ===== PASSENGER (E) =====
+
+-- ===== RESERVATION (C/D) =====
+
+-- ===== REPORT QUERIES (F) =====
